5eca23b73c1dd91254885e415df575a61a83cefc
harshkr1562 <hk122996@gmail.com>
2022-12-04 01:36:02 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+app.set('view engine' , 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+let newItems = [];
+app.get("/" , (req , res) =>{
+
+    let options = {weekday: "long" , year: "numeric" , month: "long"
+    , day: "numeric" };
+    let today = new Date();
+    let day = today.toLocaleDateString("en-US" , options);
+    
+    
+    res.render("list" , {KindOfDay: day , newListItems: newItems});
+});
+
+app.post('/' , (req , res)=>{
+    let newItem = req.body.newItem;
+    const id = Math.floor(Math.random()*10000).toString();
+    newItems.push({id, newItem});
+    res.redirect('/');
+})
+
+app.get("/delete/:id", (req,res)=>{
+    newItems = newItems.filter(newItem=>newItem.id !== req.params.id)
+    res.redirect("")
+})
+
+
+app.listen(3000 , ()=> console.log("port is running at server 3000"));
